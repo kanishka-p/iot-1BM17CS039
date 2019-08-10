8bc81a34e40f20d00993f74e33a95330759286a8
kanishka-p <53854647+kanishka-p@users.noreply.github.com>
2019-08-10 12:59:50 +0530

    Add files via upload

---
+++ led.ino.ino
@@ -0,0 +1,16 @@
+int green=2;
+int yellow=3;
+int red=4;
+// the number of the LED pin
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(green, OUTPUT);
+pinMode(yellow, OUTPUT);
+pinMode(red, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ delay(DELAY_GRee
